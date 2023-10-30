7678bddd83927bba73e786b6f2248ffa553b73c4
Wenwen Wang <wenwen.wangwen@gmail.com>
2023-10-30 13:14:53 -0700

    Ready for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path'); 
+const path = require('path');
 module.exports = {
-  mode:"development",
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       {
-        test:/\.css$/,
-        use:['style-loader', 'css-loader']
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
-      { 
-        test: /\.m?js$/, 
+      {
+        test: /\.m?js$/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react' ]
-          }
-        }
-      }
-    ]
-  }
-}; 
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
